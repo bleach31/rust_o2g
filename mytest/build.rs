da923204268403855ae245fc16976cda8d9fefd1
Motohiro Shibakawa <motohiro.shibakawa.j7r@jp.denso.com>
2021-12-08 20:56:29 +0900

    for public

--- mytest/build.rs
+++ mytest/build.rs
@@ -1,3 +1,5 @@
-println!("cargo:rustc-link-search=native={}", "./lib");
-println!("cargo:rustc-link-search=native={}", "/path/to/bootstrapperdll");
-$HOME/.nuget/packages/runtime.[runtime identifier].microsoft.dotnet.ilcompiler/[version]/sdk
+
+fn main() {
+    println!("cargo:rustc-link-search=native={}", "./lib");
+    println!("cargo:rustc-link-search=native={}", "C:/Users/{***Change***}/.nuget/packages/runtime.win-x64.microsoft.dotnet.ilcompiler/6.0.0-rc.1.21420.1/sdk");
+}
